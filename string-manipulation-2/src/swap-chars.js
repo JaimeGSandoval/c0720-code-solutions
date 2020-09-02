b671f8ca08a9bb63525e65fc88b1998499ed4075
jaimegsandoval <jaime.devs@gmail.com>
2020-09-01 17:09:51 -0700

    Working on last problem

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,8 +1,14 @@
 /* eslint-disable no-unused-vars */
 
 function swapChars(firstIndex, secondIndex, string) {
-  let firstSwap = '';
-  const output = '';
-  firstSwap = string.replace(string.charAt(firstIndex), string.charAt(secondIndex));
-  firstSwap.charAt(secondIndex).replace(string.charAt(firstIndex));
+  let output = '';
+  for (let i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      string[i] = string[i].charAt(secondIndex);
+    } else if (i === secondIndex) {
+      string[i] = string[i].charAt(firstIndex);
+    }
+    output += string[i];
+  }
+  return output;
 }
