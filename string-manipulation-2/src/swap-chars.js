a1f0af2739846d2a543169fbed0bf4612360863e
jaimegsandoval <jaime.devs@gmail.com>
2020-09-01 21:16:04 -0700

    Completed the string manipulation 2 exercise

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,14 +1,9 @@
 /* eslint-disable no-unused-vars */
 
 function swapChars(firstIndex, secondIndex, string) {
-  let output = '';
-  for (let i = 0; i < string.length; i++) {
-    if (i === firstIndex) {
-      string[i] = string[i].charAt(secondIndex);
-    } else if (i === secondIndex) {
-      string[i] = string[i].charAt(firstIndex);
-    }
-    output += string[i];
-  }
-  return output;
+  const splitStr = string.split('');
+  const tempVal = splitStr[firstIndex];
+  splitStr[firstIndex] = splitStr[secondIndex];
+  splitStr[secondIndex] = tempVal;
+  return splitStr.join('');
 }
