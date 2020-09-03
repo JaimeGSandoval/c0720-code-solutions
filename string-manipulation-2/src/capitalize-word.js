18a46afaaec721a147257a87fa09b256ecf66302
jaimegsandoval <jaime.devs@gmail.com>
2020-09-02 21:22:48 -0700

    Made the requested changes

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -4,11 +4,13 @@ function capitalizeWord(word) {
   const output = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
   const split = output.split('');
   let finalOutput = '';
+
   for (let i = 0; i < split.length; i++) {
     if (split[i] === 's') {
       split[i] = split[i].toUpperCase();
     }
     finalOutput += split[i];
   }
+
   return finalOutput;
 }
