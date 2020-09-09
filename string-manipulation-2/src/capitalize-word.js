a2c26135b87f87e69fd45df03b13c739ecd25a45
jaimegsandoval <jaime.devs@gmail.com>
2020-09-08 18:32:08 -0700

    Made changes to check for lowercased javascript string

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -2,15 +2,10 @@
 
 function capitalizeWord(word) {
   const output = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  const split = output.split('');
-  let finalOutput = '';
-
-  for (let i = 0; i < split.length; i++) {
-    if (split[i] === 's') {
-      split[i] = split[i].toUpperCase();
-    }
-    finalOutput += split[i];
+  let customForJavascript = '';
+  if (output.toLowerCase() === 'javascript') {
+    customForJavascript = 'JavaScript';
+    return customForJavascript;
   }
-
-  return finalOutput;
+  return output;
 }
