a18a80b21fda36ef48021f9e23688d2ab5e73cd6
jaimegsandoval <jaime.devs@gmail.com>
2020-09-15 16:08:15 -0700

    Completed the react rendering lists exercise

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function ListItem(props) {
+  return <li>{props.pokemon}</li>;
+}
+
+function PokemonList(props) {
+  const pokeNames = props.names;
+  const listItems = pokeNames.map(obj => {
+    return <ListItem key={obj.number} pokemon={obj.name} />;
+  });
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+ReactDOM.render(<PokemonList names={pokedex} />, document.getElementById('root'));
