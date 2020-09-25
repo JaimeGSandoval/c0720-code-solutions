eda8445bb40042836f49d96459f1aa5d4e74ee56
jaimegsandoval <jaime.devs@gmail.com>
2020-09-24 18:21:03 -0700

    Completed requested changes

--- sql-join/big-spenders.sql
+++ sql-join/big-spenders.sql
@@ -1,4 +1,7 @@
-  select "customers" as "customer",
-         "payments" as "payment"
-         from "customers"
-         join "payments" using ("customerId");
+  select "customers"."firstName" as "firstName",
+         "customers". "lastName" as "lastName",
+         "payments"."amount" as "payment"
+    from "customers"
+    join "payments" using ("customerId")
+    order by "payment" desc
+    limit 10;
