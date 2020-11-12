77f6c20e60e82b3224e3e92b1ae3c50d42b2aaa2
jaimegsandoval <jaime.devs@gmail.com>
2020-11-11 20:12:40 -0800

    Initial commit

---
+++ react-validate-input/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import ValidatedInput from './validted-input';
+
+ReactDom.render(<ValidatedInput />, document.getElementById('root'));
---
+++ react-validate-input/src/validted-input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ValidatedInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Bankai</h1>
+      </div>
+    );
+  }
+}
+
+export default ValidatedInput;
